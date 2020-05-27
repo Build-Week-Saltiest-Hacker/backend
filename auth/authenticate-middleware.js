c20882d5c0a732878ff1ce43e97090680f314342
James <jlc6290@gmail.com>
2020-05-26 19:37:38 -0500

    Server initialized, auth endpoints added and userDB migrated

---
+++ auth/authenticate-middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
+module.exports = function verifyUser(req, res, next) {
+    const token = req.headers.authorization;
+    if(token){
+        jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+            if(error){
+                res.status(401).json({ message: 'invalid token'});
+            }else{
+                req.username = decodedToken.username;
+                next();
+            }
+        })
+    }else{
+        res.status(401).json({ message: 'Unauthorized'});
+    }
+}
---
+++ data/seeds/00-users.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcryptjs');
+
+const password = 'lkajsdo9232398'
+
+hash = bcrypt.hashSync(password, 10)
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username: 'JLC6290', password: 'password'},
+      ]);
+    });
+};
